28c36d408b8eaabb7e0d0fc3045c7af7417bf78b
alpha18603074401@gmail.com <xiwang1992>
2018-01-15 21:29:31 +0800

    添加一个多线程程序

---
+++ src/main/java/com/fan/threadExplore/ThreadPool.java
@@ -0,0 +1,12 @@
+package com.fan.threadExplore;
+
+/**
+ * @author:fanwenlong
+ * @date:2018-01-15 21:28:57
+ * @E-mail:alpha18603074401@gmail.com
+ * @mobile:186-0307-4401
+ * @description:
+ * @detail:
+ */
+public interface ThreadPool {
+}
