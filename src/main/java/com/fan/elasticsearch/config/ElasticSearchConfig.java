e743088c1d60f8ad219b57524fb0103f0647aee5
alpha <Alpha18603074401@gmail.com>
2018-05-11 14:39:11 +0800

    一次并无意义的修改

---
+++ src/main/java/com/fan/elasticsearch/config/ElasticSearchConfig.java
@@ -0,0 +1,11 @@
+package com.fan.elasticsearch.config;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.Transport;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ElasticSearchConfig {
+}
